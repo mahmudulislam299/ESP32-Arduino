062fd8b33c4f29be1790d6fb4e2fe03235cbc574
mahmudulislam299 <mahmudulislam299@gmail.com>
2023-03-25 17:34:49 +0600

    new code added

--- Display/OLED_1.3_SH1106_128x64_i2c_QTPY/OLED_1.3_SH1106_128x64_i2c_QTPY.ino
+++ Display/OLED_1.3_SH1106_128x64_i2c_QTPY/OLED_1.3_SH1106_128x64_i2c_QTPY.ino
@@ -1,4 +1,4 @@
-/*********************************************************************
+ /*********************************************************************
   This is an example for our Monochrome OLEDs based on SH110X drivers
 
   This example is for a 128x64 size display using I2C to communicate
@@ -72,103 +72,103 @@ void setup()   {
   display.begin(i2c_Address, true); // Address 0x3C default
  //display.setContrast (0); // dim display
  
-  display.display();
-  delay(2000);
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  // draw a single pixel
-  display.drawPixel(10, 10, SH110X_WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw many lines
-  testdrawline();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw rectangles
-  testdrawrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw multiple rectangles
-  testfillrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw mulitple circles
-  testdrawcircle();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw a SH110X_WHITE circle, 10 pixel radius
-  display.fillCircle(display.width() / 2, display.height() / 2, 10, SH110X_WHITE);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testfillroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawtriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  testfilltriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-  testdrawchar();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-
-
-  // text display tests
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-  display.println("Failure is always an option");
-  display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
-  display.println(3.141592);
-  display.setTextSize(2);
-  display.setTextColor(SH110X_WHITE);
-  display.print("0x"); display.println(0xDEADBEEF, HEX);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // miniature bitmap display
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-  delay(1);
-
-  // invert the display
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
-  display.clearDisplay();
-
-  // draw a bitmap icon and 'animate' movement
-  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+//  display.display();
+//  delay(2000);
+//
+//  // Clear the buffer.
+//  display.clearDisplay();
+//
+//  // draw a single pixel
+//  display.drawPixel(10, 10, SH110X_WHITE);
+//  // Show the display buffer on the hardware.
+//  // NOTE: You _must_ call display after making any drawing commands
+//  // to make them visible on the display hardware!
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw many lines
+//  testdrawline();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw rectangles
+//  testdrawrect();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw multiple rectangles
+//  testfillrect();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw mulitple circles
+//  testdrawcircle();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw a SH110X_WHITE circle, 10 pixel radius
+//  display.fillCircle(display.width() / 2, display.height() / 2, 10, SH110X_WHITE);
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testdrawroundrect();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testfillroundrect();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testdrawtriangle();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testfilltriangle();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // draw the first ~12 characters in the font
+//  testdrawchar();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//
+//
+//  // text display tests
+//  display.setTextSize(1);
+//  display.setTextColor(SH110X_WHITE);
+//  display.setCursor(0, 0);
+//  display.println("Failure is always an option");
+//  display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
+//  display.println(3.141592);
+//  display.setTextSize(2);
+//  display.setTextColor(SH110X_WHITE);
+//  display.print("0x"); display.println(0xDEADBEEF, HEX);
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  // miniature bitmap display
+//  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+//  display.display();
+//  delay(1);
+//
+//  // invert the display
+//  display.invertDisplay(true);
+//  delay(1000);
+//  display.invertDisplay(false);
+//  delay(1000);
+//  display.clearDisplay();
+//
+//  // draw a bitmap icon and 'animate' movement
+//  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
 }
 
 
--- Serial_UART/Serial_2_test/Serial_2_test.ino
+++ Serial_UART/Serial_2_test/Serial_2_test.ino
@@ -1,13 +1,24 @@
-#define RXD1 9
-#define TXD1 10
+//#define RXD1 9
+//#define TXD1 10
 #define RXD2 16
 #define TXD2 17
+
+// this is random pin for serial1 test
+// this pin works fine for serial1
+//#define RXD1 26 
+//#define TXD1 27
+
+// this another test pin for serial1 test
+#define RXD1 12 
+#define TXD1 13
+
 
 void setup() {
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
   Serial.begin(115200);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+//  Serial1.begin(250000, SERIAL_8N1, RXD1, TXD1);
+//  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    Serial2.begin(250000, SERIAL_8N1, RXD1, TXD1);
   Serial.println("Serial Txd is on pin: "+String(TX));
   Serial.println("Serial Rxd is on pin: "+String(RX));
 }
@@ -16,4 +27,8 @@ void loop() { //Choose Serial1 or Serial2 as required
   while (Serial2.available()) {
     Serial.print(char(Serial2.read()));
   }
+
+//  while (Serial1.available()) {
+//    Serial.print(char(Serial1.read()));
+//  }
 }
